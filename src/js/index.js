9b4c48dcc08947395134a07477ed53d0b65571ec
Michał Krawczyk <michal.krawczyk10@gmail.com>
2021-02-04 20:43:39 +0100

    feat: add query selectors on buttons

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,29 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
-import {registerSW} from './pwa.js';
+import { registerSW } from "./pwa.js";
 registerSW();
 
 /* place your code below */
 
-const frameFirst = document.querySelector('.frame__first');
+const frameFirst = document.querySelector(".frame__first");
+const frameWorkout = document.querySelector(".frame__workout");
+const framehistory = document.querySelector('.history--js');
+const frameSettings = document.querySelector('.settings--js');
+
+
+const start = document.querySelector(".start--js");
+const history = document.querySelector(".history--js");
+const settings = document.querySelector(".settings--js");
+const go = document.querySelector(".go--js");
+const modify = document.querySelector("modify--js");
+
+start.addEventListener("click", () => {
+	frameFirst.classList.add('hide');
+  frameWorkout.classList.remove('hide');
+});
+
+history.addEventListner("click", () => {
+  frameFirst.classList.add('hide');
+  frameHistory.classList.remove('hide');
+});
