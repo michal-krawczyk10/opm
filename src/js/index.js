e6a8ad6d804be1276763d9280716de21d6f6c7e2
Michał Krawczyk <michal.krawczyk10@gmail.com>
2021-02-04 23:48:11 +0100

    feat: add js for layer 1 buttons

--- src/js/index.js
+++ src/js/index.js
@@ -4,19 +4,30 @@ import "../scss/main.scss";
 import { registerSW } from "./pwa.js";
 registerSW();
 
-/* place your code below */
+//FRAMES
+//level 1
+const frameFirst = document.querySelector(".frame__first--js");
+const frameWorkout = document.querySelector(".frame__workout--js");
+const frameHistory = document.querySelector(".frame__history--js");
+const frameSettings = document.querySelector(".frame__settings--js");
+//level 2
+const frameStandard = document.querySelector(".frame__go--js");
+const frameModify = document.querySelector(".frame__modify--js");
+const frameLevel = document.querySelector(".frame__level--js");
+const frameAction = document.querySelector(".frame__action--js");
 
-const frameFirst = document.querySelector(".frame__first");
-const frameWorkout = document.querySelector(".frame__workout");
-const framehistory = document.querySelector(".history--js");
-const frameSettings = document.querySelector(".settings--js");
-
+//BUTTONS
+//level 1
 const start = document.querySelector(".start--js");
 const history = document.querySelector(".history--js");
 const settings = document.querySelector(".settings--js");
-const go = document.querySelector(".go--js");
-const modify = document.querySelector("modify--js");
+//level 2
+const standard = document.querySelector(".go--js");
+const level = document.querySelector(".level--js");
+const modify = document.querySelector(".modify--js");
+const action = document.querySelector(".action--js");
 
+// 1 level
 start.addEventListener("click", () => {
 	frameFirst.classList.add("hide");
 	frameWorkout.classList.remove("hide");
@@ -29,11 +40,21 @@ settings.addEventListener("click", () => {
 	frameFirst.classList.add("hide");
 	frameSettings.classList.remove("hide");
 });
-go.addEventListener("click", () => {
-	frameFirst.classList.add("hide");
+
+// 2a level
+standard.addEventListener("click", () => {
+	frameWorkout.classList.add("hide");
 	frameGo.classList.remove("hide");
 });
 modify.addEventListener("click", () => {
-	frameFirst.classList.add("hide");
-	frameModify.classList.add("hide");
+	frameWorkout.classList.add("hide");
+	frameModify.classList.remove("hide");
 });
+
+//3a level
+action.addEventListener("click", () => {
+	frameGo.classList.add("hide");
+	frameAction.classList.remove("hide");
+});
+
+//3b
