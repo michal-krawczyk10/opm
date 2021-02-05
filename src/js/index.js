933c1356b828062ebb787977977ab05c4e8cff3e
Michał Krawczyk <michal.krawczyk10@gmail.com>
2021-02-05 01:20:49 +0100

    feat: add more buttons and frames, ready main branch loop

--- src/js/index.js
+++ src/js/index.js
@@ -11,10 +11,12 @@ const frameWorkout = document.querySelector(".frame__workout--js");
 const frameHistory = document.querySelector(".frame__history--js");
 const frameSettings = document.querySelector(".frame__settings--js");
 //level 2
-const frameStandard = document.querySelector(".frame__go--js");
+const frameStandard = document.querySelector(".frame__standard--js");
 const frameModify = document.querySelector(".frame__modify--js");
 const frameLevel = document.querySelector(".frame__level--js");
+//level 3
 const frameAction = document.querySelector(".frame__action--js");
+const frameDone = document.querySelector(".frame__done--js");
 
 //BUTTONS
 //level 1
@@ -22,10 +24,14 @@ const start = document.querySelector(".start--js");
 const history = document.querySelector(".history--js");
 const settings = document.querySelector(".settings--js");
 //level 2
-const standard = document.querySelector(".go--js");
+const standard = document.querySelector(".standard--js");
 const level = document.querySelector(".level--js");
 const modify = document.querySelector(".modify--js");
-const action = document.querySelector(".action--js");
+//level 3
+const go = document.querySelectorAll(".go--js");
+//level 4
+const done = document.querySelector(".done--js");
+const home = document.querySelector(".home--js");
 
 // 1 level
 start.addEventListener("click", () => {
@@ -44,17 +50,29 @@ settings.addEventListener("click", () => {
 // 2a level
 standard.addEventListener("click", () => {
 	frameWorkout.classList.add("hide");
-	frameGo.classList.remove("hide");
+	frameStandard.classList.remove("hide");
 });
 modify.addEventListener("click", () => {
 	frameWorkout.classList.add("hide");
 	frameModify.classList.remove("hide");
 });
 
-//3a level
-action.addEventListener("click", () => {
-	frameGo.classList.add("hide");
+//3 level
+go.forEach((input) => input.addEventListener("click", startGo));
+
+function startGo() {
+	frameStandard.classList.add("hide");
+	frameModify.classList.add("hide");
 	frameAction.classList.remove("hide");
+};
+
+//4 level
+done.addEventListener("click", () => {
+	frameAction.classList.add("hide");
+	frameDone.classList.remove("hide");
 });
 
-//3b
+home.addEventListener("click", () => {
+	frameDone.classList.add("hide");
+	frameFirst.classList.remove("hide");
+});
